6205bdec328d39672d65e6a59eb06746e72c3bcf
Anhad Gill <anhad@frogplum.com>
2018-11-19 15:12:56 -0800

    first commit

---
+++ spec/features/user_creates_todos_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+feature "User creates todo" do
+    scenario "successfully" do
+        
+    end
+end
---
+++ spec/features/user_visits_homepage_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+# same as describe and it combination in rpsec.
+feature "User visits homepage" do
+    scenario "successfully" do
+        # visit takes in one argument
+        visit root_path
+
+        expect(page). to have_css "h1", text: 'Todos'
+    end
+end
